b90f8ad8ed8d1a1579540b28ff995ea9ce7cedcd
NguyenDuyPhuong <nguyenduyphuong3103@gmai.com>
2023-06-19 18:12:43 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -35,6 +35,6 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 //Routes init -khởi tạo tuyến đường
 route(      app);
 
-                app.listen(port, () => {
-                    console.log(`Example app listening at http://localhost:${port}`);
-                });
+                        app.listen(port, () => {
+                            console.log(`Example app listening at http://localhost:${port}`);
+                        });
