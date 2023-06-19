30f029a40678e5c7a8a6d49a04e3eeb97ae391b8
NguyenDuyPhuong <nguyenduyphuong3103@gmai.com>
2023-06-19 18:28:11 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+                app.set('view engine', 'hbs');
+                app.set('views', path.join(__dirname, 'resources\\views'));
 
 //Routes init -khởi tạo tuyến đường
 route(app);
